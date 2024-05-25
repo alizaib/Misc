d30e068c33fffc959a49af1f9739ac79a4c81dba
alizaib <alizaib@gmail.com>
2024-05-25 21:44:06 +0500

    comments removed

--- Java/Factorial.java
+++ Java/Factorial.java
@@ -3,7 +3,6 @@ import java.lang.*;
 import java.io.*;
 import java.util.Scanner;
 
-// The main method must be in a class named "Main".
 public class Factorial {
     public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
